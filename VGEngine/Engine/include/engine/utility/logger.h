2dd6ba8f64cc191dcdddb7458e086d17fa56b948
havyton <tero@ebola.dead>
2015-02-19 12:41:32 +0200

    Reduced size of logger

--- VGEngine/Engine/include/engine/utility/logger.h
+++ VGEngine/Engine/include/engine/utility/logger.h
@@ -1,11 +1,4 @@
 #pragma once
 #include <android\log.h>
-namespace vg
-{
-#define Log(tag, message, ...) __android_log_print(ANDROID_LOG_DEBUG, tag, "File: %s, Line: %d, Method: %s, Message: " message, __FILE__, __LINE__, __FUNCTION__, __VA_ARGS__)
-	class Logger
-	{
-	public:
-	};
 
-}
+#define Log(tag, message, ...) __android_log_print(ANDROID_LOG_DEBUG, tag, "File: %s, Line: %d, Method: %s, Message: " message, __FILE__, __LINE__, __FUNCTION__, __VA_ARGS__)
