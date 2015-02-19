2ba9da83419a009f84a1d5c19acd006c69492da0
Zegram <Joikkiz@hotmail.com>
2015-02-19 11:58:40 +0200

    Added template documentation comments in buffer

--- VGEngine/Engine/include/engine/graphics/buffer.h
+++ VGEngine/Engine/include/engine/graphics/buffer.h
@@ -2,23 +2,54 @@
 #include "iostream"
 namespace vg
 {
+	/**
+	Manages buffers.
+	*/
 	class buffer
 	{
 	public:
 		buffer(){};
 		~buffer(){};
 
+
 		enum bufferType { GL_ARRAY_BUFFER, GL_ELEMENT_ARRAY_BUFFER };
 		enum usageType { GL_STREAM_DRAW, GL_STATIC_DRAW, GL_DYNAMIC_DRAW };
 
+		/**
+		<description>
+		@param path <description>
+		@return <description>
+		*/
 		void generateBuffers(const unsigned int amount, unsigned int* buffers);
+
+		/**
+		<description>
+		@param path <description>
+		@return <description>
+		*/
 		void deleteBuffers(const unsigned int amount, unsigned int* buffers);
+
+		/**
+		<description>
+		@param path <description>
+		@return <description>
+		*/
 		void bindBuffer(bufferType type, unsigned int buffer);
+
+		/**
+		<description>
+		@param path <description>
+		@return <description>
+		*/
 		void unbindBuffer(bufferType type);
+
+		/**
+		<description>
+		@param path <description>
+		@return <description>
+		*/
 		void setBufferData(bufferType type, unsigned int size, void* data, usageType usagetype);
 
 	private:
-
 	};
-
 }
