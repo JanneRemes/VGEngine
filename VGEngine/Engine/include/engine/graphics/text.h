f3d172dca551cb647ed85e32139fbd08a16548d9
Zegram <TTI13SJoelM@kao.fi>
2015-03-12 13:56:53 +0200

    Text class does not break anymore, will test later.

---
+++ VGEngine/Engine/include/engine/graphics/text.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "engine\graphics\stb_truetype.h"
+#include <GLES2/gl2.h>
+#include "engine\assets\fileManager.h"
+
+namespace vg
+{
+	class text
+	{
+	public:
+		~text(){};
+
+		text(std::string& fontPath, FileManager *manager);
+		void print(float x, float y, char* text);
+
+	private:
+		const unsigned char *mBuffer;
+		unsigned char mBitmap[512*512];
+		const char *mFontPath;
+		std::string mOutData;
+		GLuint ftex;
+		stbtt_bakedchar mData[96];
+		void convertTriangles();
+		
+	};
+}
