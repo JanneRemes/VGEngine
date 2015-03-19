33fb37d74818c8ea4f0e709272c115c0d39b3402
Zegram <TTI13SJoelM@kao.fi>
2015-03-19 13:52:31 +0200

    Added sufficient documentation for sceneManager, added documentation template for text.h & buffer.h

--- VGEngine/Engine/sceneManager.h
+++ VGEngine/Engine/sceneManager.h
@@ -5,48 +5,52 @@ using namespace std;
 namespace vg
 {
 	/**
-	<description>
+	Manages Scenes.
 	*/
 	class sceneManager
 	{
 	public:
+		/**
+		Initializes default values
+		*/
 		sceneManager();
 		~sceneManager();
 
+		/**
+		Draws the current scene
+		*/
 		void draw();
 
 		/**
-		<description>
-		@param dt <description>
-		@return <description>
+		Draws the current scene that isnt paused
+		@param dt The elapsed time since the game last updated
 		*/
 		void update(float dt);
 
 		/**
-		<description>
-		@param scene <description>
-		@return <description>
+		Opens a scene, pushes it to the currentScene vector
+		@param scene the scene which you want to show on the screen
 		*/
 		void openScene(Scene *scene);
 
 		/**
-		<description>
-		@param scene <description>
-		@return <description>
+		Changes the scene to the scene given for this function
+		@param scene the scene which you want to show on the screen
 		*/
-
 		void changeScene(Scene *scene);
 
 		/**
-		<description>
-		@param scene <description>
-		@return <description>
+		Completely empties the currentScene vector from all scenes
 		*/
 		void emptyScenes();
+
+		/**
+		Closes the scene that is currently playing
+		*/
 		void closeCurrentScene();
 
 	private:
-		vector<Scene*> currentScenes;
-		bool sceneChanged = false;
+		vector<Scene*> currentScenes; ///< Vector that includes all the scenes that we are currently using
+		bool sceneChanged = false; ///< Bool that is used for changing scenes.
 	};
 }
