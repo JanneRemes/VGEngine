7684ffd2e05f448cca179ebe73f1f841f46c41c1
zeropointx <tti13smiikas@kamk.fi>
2015-09-28 22:36:46 +0300

    Win32 Starts (but doesn't do anything)! All projects linked correctly (I hope). Still need to add windows spesific files.

--- VGEngine/Windows Game/Main.cpp
+++ VGEngine/Windows Game/Main.cpp
@@ -1,13 +1,13 @@
 
 #include <engine\game\game.h>
 
-extern void main_dummy();
+//extern void main_dummy();
 using namespace vg;
 using namespace vg::graphics;
 void test_dummy()
 {
 
-	main_dummy();
+	//main_dummy();
 }
 void mainGame(Game* game)
 {
