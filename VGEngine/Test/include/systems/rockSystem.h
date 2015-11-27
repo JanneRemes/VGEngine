6913a23509cb2ab065730caee1d360277994cf24
Zegram <Joikkiz@hotmail.com>
2015-11-27 13:35:41 +0200

    Added more stuff to rockThrowScene

--- VGEngine/Test/include/systems/rockSystem.h
+++ VGEngine/Test/include/systems/rockSystem.h
@@ -7,6 +7,8 @@
 #include "engine/utility/timer.h"
 #include "engine/game/physicsSystem.h"
 
+enum Background { BACKGROUND1, BACKGROUND2, RESET };
+
 using namespace vg;
 class rockSystem :
 	public System
@@ -15,11 +17,19 @@ public:
 	rockSystem(Scene *scene);
 	~rockSystem();
 	void update(std::vector<vg::GameObject*> *gameObjects,float deltaTime);
+	void updateBars();
+	void updateExplosives(int amount, std::vector<vg::GameObject*> *gameObjects);
+	void updateInputs();
+	void updateView();
 	
 private:
 	PhysicsSystem *system;
 
 	Scene *scene;
+
+	GameObject *background1;
+	GameObject *background2;
+
 	GameObject *rock;
 
 	GameObject *bar1;
@@ -28,6 +38,8 @@ private:
 	GameObject *indicator1;
 	GameObject *indicator2;
 
+	GameObject *explosive;
+
 	int height, minHeight, maxHeight;
 	bool heightLock, goUp, goDown;
 
@@ -35,5 +47,11 @@ private:
 	bool powerLock, powerUp, powerDown;
 
 	bool shot;
+	int i;
+
+	enum Background bgState;
+	TransformComponent* currentBackground;
+
+	Timer timer;
 };
 
